3a5f71fc65b6510d4b6ceb40a97a0633195560d8
rtipnis <rtipnis@gmail.com>
2024-04-21 23:22:07 -0400

    Update test.js

--- test.js
+++ test.js
@@ -1,7 +1,4 @@
 <script>
   // JavaScript code to test rendering
-  document.addEventListener("DOMContentLoaded", function() {
-    var renderingTestDiv = document.getElementById("renderingTest");
-    renderingTestDiv.innerHTML = "<h1>This is a rendering test!</h1><p>Your HTML content is rendering correctly.</p>";
-  });
+  document.write("<h1>This is a rendering test!</h1><p>Your HTML content is rendering correctly.</p>");
 </script>
